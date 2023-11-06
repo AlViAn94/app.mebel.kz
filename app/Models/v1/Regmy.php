1c85ff8d7ef587e541329ad68325a4e17a9afaff
Aleksandr An <vr_kz@bk.ru>
2023-11-06 12:55:21 +0500

    refactor regmy 1

--- app/Models/v1/Regmy.php
+++ app/Models/v1/Regmy.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 use Illuminate\Support\Facades\File;
-use Illuminate\Http\UploadedFile;
 
 class Regmy extends Model
 {
@@ -32,20 +31,6 @@ class Regmy extends Model
         $date_time = $date->format('Y-m-d_H:m:s');
         $date_add = $date->format('Y-m-d');
 
-        $existingRecord = self::where('user_id', $user['id'])
-            ->where('created_at', 'LIKE', '%' . $date_add . '%')
-            ->count();
-        switch ($existingRecord){
-            case 0:
-                $action = 'entrance';
-            break;
-            case 1:
-                $action = 'exit';
-            break;
-            default:
-                return response()->json(['message' => 'Вы уже зарегистрировали вход и выход за сегодня.'], 404);
-        }
-
         if (!File::isDirectory($directoryPath)) {
             File::makeDirectory($directoryPath, 0755, true, true);
         }
@@ -58,31 +43,46 @@ class Regmy extends Model
         File::put($filePath, file_get_contents($file));
         $fileLink = env('APP_URL') . ('/downloads/files/regmy/' . $fileName);
 
-        if (File::exists($filePath)) {
-            $result = self::insert([
-                'user_id' => $user['id'],
-                'action' => $action,
-                'name' => $user['name'],
-                'file' => $fileLink
-            ]);
+        $existingRecord = self::where('user_id', $user['id'])
+            ->where('created_at', 'LIKE', '%' . $date_add . '%')
+            ->count();
 
-            if (!$result) {
-                File::delete($filePath);
-                return response()->json(['message' => 'Не верный запрос.'], 404);
+        if (File::exists($filePath)) {
+            switch ($existingRecord){
+                case 0:
+                        $result = self::insert([
+                            'user_id' => $user['id'],
+                            'name' => $user['name'],
+                            'entrance_time' => $date_time,
+                            'entrance_file' => $fileLink
+                        ]);
+                    if (!$result) {
+                        File::delete($filePath);
+                        return response()->json(['message' => 'Не верный запрос.'], 404);
+                    }
+                    return response()->json(['action' => 'entrance']);
+                case 1:
+                    $result = self::where('user_id', $user['id'])
+                        ->where('entrance_time', 'LIKE', '%' . $date_add . '%')
+                        ->update([
+                        'exit_time' => $date_time,
+                        'exit_file' => $fileLink
+                    ]);
+                    if (!$result) {
+                        File::delete($filePath);
+                        return response()->json(['message' => 'Не верный запрос.'], 404);
+                    }
+                    return response()->json(['action' => 'exit']);
+                default:
+                    return response()->json(['message' => 'Вы уже зарегистрировали вход и выход за сегодня.'], 404);
             }
         }
-        if($action == 'entrance'){
-            return response()->json(['action' => 'entrance']);
-
-        }else{
-            return response()->json(['action' => 'exit']);
-        }
+        return response()->json(['message' => 'Не верный запрос.'], 404);
     }
 
     public static function getList($data)
     {
         $period = $data['period'];
-        $action = $data['action'];
         $search = $data['search'];
         $sort = $data['sort'];
         $asc = $data['asc'];
@@ -115,10 +115,6 @@ class Regmy extends Model
             ->where(function ($query) use ($search) {
                 $query->where('name', 'LIKE', "%{$search}%");
             });
-
-        if ($action !== null) {
-            $query->where('action', $action);
-        }
 
         $existingRecord = $query
             ->orderBy($sort, $asc ? 'asc' : 'desc')
