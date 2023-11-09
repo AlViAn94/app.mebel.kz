5766d82c9e996f2d24207d333b9b2c3d211f73be
Aleksandr An <vr_kz@bk.ru>
2023-11-09 10:00:22 +0500

    refactor regmy 11

--- app/Http/Controllers/v1/File/FileController.php
+++ app/Http/Controllers/v1/File/FileController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\v1\File;
 
 use App\Http\Controllers\Controller;
+use App\Models\v1\Role;
 use App\Services\v1\File\DeletedFileService;
 use App\Services\v1\File\DownloadFileService;
 use App\Services\v1\File\SaveFileService;
 use App\Services\v1\File\UpdateFileService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
 class FileController extends Controller
@@ -42,6 +44,17 @@ class FileController extends Controller
 
     public function update(Request $request, UpdateFileService $service)
     {
+        $dir = $request->input('dir');
+        $id = $request->input('id');
+
+        $user = Auth::user();
+        $user_id = $user['id'];
+        $roles = Role::getPositions($user_id);
+
+        if (!in_array($dir, $roles)) {
+            return response()->json(['message' => 'Только начальник цеха может удалить карту.'], 404);
+        }
+
         $files = [];
         $index = 0;
         // Получите файлы и другие параметры
@@ -50,8 +63,7 @@ class FileController extends Controller
             $files[$fileKey] = $request->file($fileKey);
             $index++;
         }
-        $dir = $request->input('dir');
-        $id = $request->input('id');
+
 
         // Ваш код обработки файлов и параметров
         $result = $service->updateFiles($files, $dir, $id);
--- app/Http/Controllers/v1/Order/Job/Factory/FactoryCardController.php
+++ app/Http/Controllers/v1/Order/Job/Factory/FactoryCardController.php
@@ -15,7 +15,7 @@ class FactoryCardController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request, AddNewCard $service)
     {
