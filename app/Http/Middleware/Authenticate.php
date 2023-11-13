ea98bce6dd482deee31ddfbe12628c4f46c83239
Aleksandr An <vr_kz@bk.ru>
2023-11-13 09:56:00 +0500

    refactor token

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -23,32 +23,39 @@ class Authenticate extends Middleware
      * Время жизни токена 48 часов
      * Если токену от 23 до 48 часов он отдаст новый токен
      */
-    public function handle($request, Closure $next, ...$guards)
-    {
-        try {
-            $token = JWTAuth::getToken();
+//    public function handle($request, Closure $next, ...$guards)
+//    {
+//        try {
+//            $token = JWTAuth::getToken();
+//
+//            if ($token) {
+//                $payload = JWTAuth::getPayload($token)->toArray();
+//                $issuedAt = $payload['iat'];
+//                $tokenAge = time() - $issuedAt;
+////                    if ($tokenAge > 60 * 60 * 23 && $tokenAge <= 60 * 60 * 48) {
+//                    if ($tokenAge > 20 && $tokenAge <= 60) {
+//                        try {
+//                            $user = Auth::user();
+//                            $user = User::chechEmail($user['email']);
+//                            if (! $token = JWTAuth::fromUser($user)) {
+//                                return response()->json(['message' => 'Could not create token'], 500);
+//                            }
+//                            return response()->json(['access_token' => $token], 407);
+//                        } catch (\Exception $e) {
+//                            return response()->json(['message' => $e], 403);
+//                        }
+//                    }
+//            }
+//        } catch (TokenExpiredException $e) {
+//            return response()->json(['message' => $e], 403);
+//        }
+//        return parent::handle($request, $next, ...$guards);
+//    }
 
-            if ($token) {
-                $payload = JWTAuth::getPayload($token)->toArray();
-                $issuedAt = $payload['iat'];
-                $tokenAge = time() - $issuedAt;
-//                    if ($tokenAge > 60 * 60 * 23 && $tokenAge <= 60 * 60 * 48) {
-                    if ($tokenAge > 20 && $tokenAge <= 60) {
-                        try {
-                            $user = Auth::user();
-                            $user = User::chechEmail($user['email']);
-                            if (! $token = JWTAuth::fromUser($user)) {
-                                return response()->json(['message' => 'Could not create token'], 500);
-                            }
-                            return response()->json(['access_token' => $token], 407);
-                        } catch (\Exception $e) {
-                            return response()->json(['message' => $e], 403);
-                        }
-                    }
-            }
-        } catch (TokenExpiredException $e) {
-            return response()->json(['message' => $e], 403);
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
         }
-        return parent::handle($request, $next, ...$guards);
     }
 }
