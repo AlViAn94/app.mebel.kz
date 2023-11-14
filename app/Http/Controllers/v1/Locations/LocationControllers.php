4e8761a2cde8a187ca8f38f70c580d36f44e8aa9
Aleksandr An <vr_kz@bk.ru>
2023-11-14 09:58:42 +0500

    fix location 4

--- app/Http/Controllers/v1/Locations/LocationControllers.php
+++ app/Http/Controllers/v1/Locations/LocationControllers.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\v1\Locations;
 
 use App\Http\Controllers\Controller;
-use App\Models\v1\Raion;
+use App\Models\v1\District;
 use Illuminate\Http\Request;
 
 class LocationControllers extends Controller
 {
     public function list(Request $request)
     {
-        return Raion::list($request->all());
+        return District::list($request->all());
     }
 }
