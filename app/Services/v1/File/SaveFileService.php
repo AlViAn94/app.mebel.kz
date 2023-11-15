6fd10e2b890c34283cf2abd84f5d7cc32bd63076
Aleksandr An <vr_kz@bk.ru>
2023-11-15 09:35:39 +0500

    refactor file save 9

--- app/Services/v1/File/SaveFileService.php
+++ app/Services/v1/File/SaveFileService.php
@@ -60,7 +60,7 @@ class SaveFileService
         $i = 0;
         foreach ($files as $file){
             $extension = $file->getClientOriginalExtension();
-            $file_name = Str::random(15) . $extension;
+            $file_name = Str::random(15) . '.' .$extension;
             $file_path = $save_path . $file_name;
 
             if (!File::exists($save_path)) {
